d8f2038a3842442fc4ded219163d1f4df36cce9e
Matthew Bull <matt_wings@hotmail.com>
2017-07-19 09:49:10 +0100

    Better test description

--- test/schedule.Spec.js
+++ test/schedule.Spec.js
@@ -5,7 +5,7 @@ describe('schedule', () => {
   it('returns null when a meeting cannot be scheduled', () =>
     expect(schedule([['9:00', '19:00']], 60)).to.be.null
   );
-  it('returns the first time a single person could make a meeting', () =>
+  it('returns the start of the day for a single empty calendar', () =>
     expect(schedule([[]]), 60).to.equal('9:00')
   );
 });
