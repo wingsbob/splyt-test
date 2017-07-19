3508fbd57b2051592100c49b614825be37dc2695
Matthew Bull <matt_wings@hotmail.com>
2017-07-19 13:39:55 +0100

    Helpers is a terrible name. Removed the extra file, as we're below 50 lines for the solution now

--- src/schedule.js
+++ src/schedule.js
@@ -1,7 +1,15 @@
-const {
-  parseTime,
-  formatTime
-} = require('./helpers');
+const parseTime = time => {
+  const [hours, minutes] = time.split(':').map(n => parseInt(n, 10));
+  return 60 * hours + minutes;
+};
+
+const formatTime = totalMinutes => {
+  if (totalMinutes === null) return null;
+  const minutes = totalMinutes % 60;
+  const hours = (totalMinutes - minutes) / 60;
+
+  return `${hours}:${minutes >= 10 ? minutes : `0${minutes}`}`;
+};
 
 const endOfDay = parseTime('19:00');
 
