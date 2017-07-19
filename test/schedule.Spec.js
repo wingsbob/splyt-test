0c0b5a9c2c3453a442c2bea81c65ce24e700f2aa
Matthew Bull <matt_wings@hotmail.com>
2017-07-19 09:51:59 +0100

    Fixed schema for schedule to be 3-d

--- test/schedule.Spec.js
+++ test/schedule.Spec.js
@@ -3,7 +3,7 @@ const schedule = require('../src/schedule');
 
 describe('schedule', () => {
   it('returns null when a meeting cannot be scheduled', () =>
-    expect(schedule([['9:00', '19:00']], 60)).to.be.null
+    expect(schedule([[['9:00', '19:00']]], 60)).to.be.null
   );
   it('returns the start of the day for a single empty calendar', () =>
     expect(schedule([[]]), 60).to.equal('9:00')
